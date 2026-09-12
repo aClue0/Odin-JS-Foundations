ac5a36665f01afe1af88f970842b1bd8567dd060
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-13 02:55:22 +0300

    Trying the new lazygit setup

--- coding-along.js
+++ coding-along.js
@@ -2,4 +2,3 @@ const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
 const filtered = cats.filter((cat) => cat.startsWith("L"));
 console.log(filtered);
-// [ "Leopard", "Lion" ]
