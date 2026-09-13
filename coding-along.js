b64d1f013d93de49f917203abdafea41b990305e
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-13 03:08:22 +0300

    Practice the syntax of arrow function and filter

--- coding-along.js
+++ coding-along.js
@@ -1,4 +1,6 @@
 const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
-const filtered = cats.filter((cat) => cat.startsWith("L"));
+const filtered = cats.filter((cat) => {
+  return cat.startsWith("L");
+});
 console.log(filtered);
