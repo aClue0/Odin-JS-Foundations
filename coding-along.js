174bb719391dd995254396378a95d81392afbc62
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-13 03:30:49 +0300

    Practicing in index.html

--- coding-along.js
+++ coding-along.js
@@ -1,6 +1,5 @@
 const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
-const filtered = cats.filter((cat) => {
-  return cat.startsWith("L");
-});
-console.log(filtered);
+for (const cat of cats) {
+  console.log(cat);
+}
