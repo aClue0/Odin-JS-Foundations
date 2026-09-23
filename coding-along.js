c4772ddcfd4c4acf82ca2e0e950320880c0d5f42
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-23 07:57:27 +0300

    Add reduce method for summation

--- coding-along.js
+++ coding-along.js
@@ -2,8 +2,8 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 let trippledEven = arr
   .filter((num) => num % 2 === 0)
-
-  .map((num) => num * 3);
+  .map((num) => num * 3)
+  .reduce((sum, current) => sum + current, 0);
 
 for (const num of trippledEven) {
   console.log(num);
