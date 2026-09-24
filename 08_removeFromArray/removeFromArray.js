8170f9285cfdb026536284491b74074cf68806f8
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-24 07:15:53 +0300

    Take in Arguments from the function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
-const removeFromArray = function (arr, numberToRemove) {
-  let index = arr.indexOf(numberToRemove);
-  arr.splice(index, 1);
+const removeFromArray = function (arr) {
+  let numbersToRemove = Array.from(arguments).slice(1);
+  console.log(numbersToRemove);
   return arr;
 };
+removeFromArray([1, 2, 3, 4, 5, 6, 7, 7], 1, 2, 3, 4, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
