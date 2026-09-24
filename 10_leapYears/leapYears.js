989d80895b604d90b58335a668c2ef0e6fca6e96
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-24 13:57:51 +0300

    Solve leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (num) {
+  isLeapYear = false;
+  if ((num % 4 === 0 && !(num % 100 === 0)) || num % 400 === 0) {
+    isLeapYear = true;
+  }
+  return isLeapYear;
 };
 
 // Do not edit below this line
