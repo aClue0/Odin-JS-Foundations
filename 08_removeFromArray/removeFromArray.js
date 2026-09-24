a6e8e31a668848517dd21d4f309aac1804504d5e
Hazem Magdy <1hazemmagdy@gmail.com>
2026-09-24 07:49:09 +0300

    Clean the code from debugging

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
 const removeFromArray = function (arr) {
   let numbersToRemove = Array.from(arguments).slice(1);
-  console.log(numbersToRemove);
   for (let i = 0; i < numbersToRemove.length; i++) {
     const num = numbersToRemove[i];
     while (arr.includes(num)) {
@@ -9,11 +8,8 @@ const removeFromArray = function (arr) {
       arr.splice(index, 1);
     }
   }
-  console.log(arr);
   return arr;
 };
-removeFromArray([1, 2, 3, 4], 7, "tacos");
-// removeFromArray([1, 2, 3, 4, 5, 6, 7, 7], 1, 2, 3, 4, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
